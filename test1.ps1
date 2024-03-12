395e99480a9cb6fd11a6ef645c56947b0e8ad7cb
Ankush-ITA <127494609+Ankush-ITA@users.noreply.github.com>
2024-03-12 23:37:38 +0530

    Add files via upload

---
+++ test1.ps1
@@ -0,0 +1,13 @@
+﻿
+
+$script = split-path -parent $MyInvocation.MyCommand.Definition
+#$script
+
+$script1= Join-Path -path $script -ChildPath "C:\Users\ankushku\OneDrive - Capgemini\Redeployement\BID283_Certificate-Expiry_Check\Scripts\MicroBots"
+
+#$script1
+
+
+$scrip3 = "$script1\Test1.txt"
+
+$checking1 =Get-Content "$scrip3"
